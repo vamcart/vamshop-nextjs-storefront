c4607581ea15d4c2507ab990c61110b93e0abcc1
VamCart <vamcart@gmail.com>
2025-02-12 14:54:47 +0300

    Добавлен домен cloudinary для отображения картинок в блоке FeaturedProducts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/dkpo0tq0m/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
