8f946faff564fd0e742509973ac4d2deb1c0f62a
VamCart <vamcart@gmail.com>
2025-02-12 14:03:11 +0300

    Footer компонент

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 import "./globals.css";
+import Footer from "@/components/footer";
 
 const font = Urbanist({
   subsets: ["latin"],
@@ -18,7 +19,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.className} antialiased`}>{children}</body>
+      <body className={`${font.className} antialiased`}>
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
