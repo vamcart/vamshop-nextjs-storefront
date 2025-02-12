07391ea04904bb6372b482104d19a25bc7c9314e
VamCart <vamcart@gmail.com>
2025-02-12 17:35:16 +0300

    Убран трюк с  isMounted, в nextjs 15 и так нет ошибки hydration

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { useEffect, useState } from "react";
-
 import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
 
@@ -11,16 +9,7 @@ import CartItem from "./components/cart-item";
 //export const revalidate = 0;
 
 const CartPage = () => {
-  const [isMounted, setIsMounted] = useState(false);
   const cart = useCart();
-
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-
-  if (!isMounted) {
-    return null;
-  }
 
   return (
     <div className="bg-white">
