29c49f357e09e57885ecfaf7b08fffb77f4a879b
VamCart <vamcart@gmail.com>
2025-02-12 14:53:41 +0300

    Добавлены компоненты для вывода товара на главной

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,4 +1,6 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
 
@@ -6,10 +8,14 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const billboard = await getBillboard("7f196f3a-ca13-4130-ad3a-6e6d704c5d9f");
+  const products = await getProducts({ isFeatured: true });
   return (
     <Container>
       <div className="space-y-10 pb-10">
         <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
       ;
     </Container>
