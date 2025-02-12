46cb8b0cbca81ba80c561a66350c06878f3d2725
VamCart <vamcart@gmail.com>
2025-02-12 14:08:41 +0300

    Добавлен компонент container

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,17 @@
 import Link from "next/link";
 
+import Container from "@/components/ui/container";
+
 const Navbar = async () => {
   return (
     <div className="border-b">
-      <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-        <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-          <p className="font-bold text-xl">STORE</p>
-        </Link>
-      </div>
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+        </div>
+      </Container>
     </div>
   );
 };
