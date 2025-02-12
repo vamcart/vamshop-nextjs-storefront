d005ec45a5e54801291e54ffa8f401fbe90abff8
VamCart <vamcart@gmail.com>
2025-02-12 14:46:00 +0300

    Добавлен action getBillboard и компонент Billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,19 @@
+import getBillboard from "@/actions/get-billboard";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
 const HomePage = async () => {
-  return <div className="space-y-10 pb-10">Hello Store</div>;
+  const billboard = await getBillboard("7f196f3a-ca13-4130-ad3a-6e6d704c5d9f");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      ;
+    </Container>
+  );
 };
 
 export default HomePage;
