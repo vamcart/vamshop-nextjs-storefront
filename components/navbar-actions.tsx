ad78ab2fc8940d55a72a2504bdb6f969844113ca
VamCart <vamcart@gmail.com>
2025-02-12 14:35:43 +0300

    Корректировка navbar

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -9,13 +9,12 @@ import Button from "@/components/ui/button";
 
 const NavbarActions = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const router = useRouter();
+  //const cart = useCart();
 
   useEffect(() => {
     setIsMounted(true);
   }, []);
-
-  const router = useRouter();
-  //const cart = useCart();
 
   if (!isMounted) {
     return null;
