f14339cf9f85dbf496b565aab90a1633335c0abd
VamCart <vamcart@gmail.com>
2025-02-12 14:21:25 +0300

    Добавлен action getCategories

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 
-import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
+import Container from "@/components/ui/container";
+//import NavbarActions from "@/components/navbar-actions";
+import getCategories from "@/actions/get-categories";
 
 const Navbar = async () => {
+  const categories = await getCategories();
+
   return (
     <div className="border-b">
       <Container>
@@ -11,7 +15,8 @@ const Navbar = async () => {
           <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
             <p className="font-bold text-xl">STORE</p>
           </Link>
-          <MainNav data={[]} />
+          <MainNav data={categories} />
+          {/* <NavbarActions /> */}
         </div>
       </Container>
     </div>
